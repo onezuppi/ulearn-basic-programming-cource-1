c573664ec5b80aa54aa54b655b70bd0ee4b57521
onezuppi <52073821+onezuppi@users.noreply.github.com>
2021-06-26 14:58:51 +0500

    Added fourth topic.

---
+++ Массивы/HeatmapTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] people)
+        {
+            var days = GetRange(2, 30);
+            var months = GetRange(1, 12);
+            var heatmapData = new double[30, 12];
+            foreach (var person in people)
+            {
+                if (person.BirthDate.Day != 1)
+                    heatmapData[person.BirthDate.Day - 2, person.BirthDate.Month - 1]++;
+            }
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                heatmapData,
+                days,
+                months);
+        }
+
+        private static string[] GetRange(int start, int count)
+        {
+            return Enumerable.Range(start, count).Select(x => x.ToString()).ToArray();
+        } 
+    }
+}
---
+++ Массивы/HistogramTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] people, string name)
+        {
+            var days = GetDayNumbers();
+            var birthdayСounts = new double[31]; 
+            foreach(var person in people )
+            {
+                if (person.Name == name && person.BirthDate.Day != 1)
+                    birthdayСounts[person.BirthDate.Day - 1]++;
+            }
+            return new HistogramData(
+                $"Рождаемость людей с именем '{name}'",
+                days,
+                birthdayСounts);
+        }
+		
+        private static string[] GetDayNumbers()
+        {
+            var days = new string[31];
+            for (int dayNumber = 1; dayNumber < 32; dayNumber++)
+                days[dayNumber - 1] = dayNumber.ToString();
+            
+            return days;
+        }
+    }
+}
